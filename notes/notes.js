721575912d35233b927683f49074d9ace83b9fc6
vince <vinceskahan@gmail.com>
2017-10-25 16:32:48 -0700

    after sec lesson 16

--- notes/notes.js
+++ notes/notes.js
@@ -5,9 +5,31 @@ console.log('starting notes.js');
 //   return 'New note';
 // };
 
+const fs = require('fs');
+
 var addNote = (title,body) => {
-  console.log('Adding note', title, body);
-};
+  var notes = [];
+  var note = {
+    title,
+    body,
+  }
+
+  // handle no file, or corrupted file
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+      notes = JSON.parse(notesString);
+  } catch (e) {
+  }
+
+  // flag this as a dup if title matches a previous note
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    // append to the file, creating it if needed
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+}; // else do nothing
 
 var getAll = () => {
   console.log('Getting all notes');
