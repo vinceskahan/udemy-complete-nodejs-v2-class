d4785114aace343b003468621e674bbcef5e3659
Vince Skahan <vds@uw.edu>
2017-11-02 08:23:05 -0700

    end of sec4/28 initial use of googlemaps api

---
+++ weather-app/app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+// see https://www.npmjs.com/package/request
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20Lombard%20Street%20Philadelphia',
+  json: true
+}, (error, response, body) => {
+  console.log(body);
+});
