eaf88a47d518d755659b5d7917cf3f54f31d7c6e
Vince Skahan <vds@uw.edu>
2017-11-02 08:30:22 -0700

    after sec4/29 - pretty print

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,5 +5,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20Lombard%20Street%20Philadelphia',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
