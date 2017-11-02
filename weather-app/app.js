c154c0a853cc1e8bc9432ff8c8743260ecea5860
Vince Skahan <vds@uw.edu>
2017-11-02 12:30:50 -0700

    stash for travel

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode.js');
+
 const argv = yargs
   .options( {
     a: {
@@ -14,23 +16,4 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedURI = encodeURIComponent(argv.address);
-
-// see https://www.npmjs.com/package/request
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedURI}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('unable to connect to server');
-  } else if (body.status === "ZERO_RESULTS") {
-    console.log('no results found');
-  } else if (body.status === "OK"){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  } else {
-    // example - if we exceed the api limit
-    console.log(`unexpected error: ${body.status}`);
-  }
-});
+geocode.geocodeAddress(argv.address);
---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,29 @@
+
+var geocodeAddress = (address) => {
+
+  var encodedURI = encodeURIComponent(address);
+
+  // see https://www.npmjs.com/package/request
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedURI}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      console.log('unable to connect to server');
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log('no results found');
+    } else if (body.status === "OK"){
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    } else {
+      // example - if we exceed the api limit
+      console.log(`unexpected error: ${body.status}`);
+    }
+  };
+
+};
+
+module.exports = {
+  geocodeAddress,
+};
