6c48fb9476c350e6107a0369809a74bfe1e47e3c
Vince Skahan <vds@uw.edu>
2017-11-02 09:12:46 -0700

    after sec4/31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options( {
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'address to fetch weather for',
+      string: true,
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedURI = encodeURIComponent(argv.address);
 
 // see https://www.npmjs.com/package/request
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20Lombard%20Street%20Philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedURI}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
