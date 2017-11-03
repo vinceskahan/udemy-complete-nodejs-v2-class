b4f792dc555e58b6f3ff500bb98bdd685b0c02ad
vince <vinceskahan@gmail.com>
2017-11-03 09:23:47 -0700

    reject without catching throws deprecation warning

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,10 +1,13 @@
 var somePromise = new Promise((resolve,reject) => {
   setTimeout(() => {
-    resolve('Hey it worked');
+//    resolve('Hey it worked');
+    reject(`ruh roh`);
   }, 2500);
 
 });
 
 somePromise.then((message) => {
   console.log('success', message);
+}, (errorMessage) => {
+  console.log('error: ', message);
 });
