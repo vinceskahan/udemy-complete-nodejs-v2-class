fd49a2979efadc80f9488b0b9181abc802d21b64
vince <vinceskahan@gmail.com>
2017-11-03 09:24:57 -0700

    fix bug (doh)

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -2,12 +2,12 @@ var somePromise = new Promise((resolve,reject) => {
   setTimeout(() => {
 //    resolve('Hey it worked');
     reject(`ruh roh`);
-  }, 2500);
+  }, 500);
 
 });
 
 somePromise.then((message) => {
   console.log('success', message);
 }, (errorMessage) => {
-  console.log('error: ', message);
+  console.log('error: ', errorMessage);
 });
