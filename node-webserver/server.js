e2337e76e9ede6d2958f378e8a97d6e5fa08d3d6
vince <vinceskahan@gmail.com>
2017-11-03 13:38:14 -0700

    initial pages

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -3,7 +3,18 @@ const express = require("express");
 var app = express();
 
 app.get('/', (request,response) => {
-  response.send('hello express');
+  //response.send('<h2>hello express</h2>');
+  response.send({
+    name: 'me',
+    likes: [
+      'item1',
+      'item2',
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('about this site');
 });
 
 app.listen(3000);
