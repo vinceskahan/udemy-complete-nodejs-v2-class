70c67de90cff6ecc44f4fbad925e20a65e94a908
vince <vinceskahan@gmail.com>
2017-11-03 13:42:23 -0700

    json or payload error message to /bad link

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -17,4 +17,16 @@ app.get('/about', (req, res) => {
   res.send('about this site');
 });
 
+// send back json with errorMessage property and content of error
+
+var errorMessage = {
+  message: "this is an error message"
+}
+app.get('/bad', (req, res) => {
+  // just the message payload
+  //    res.send(errorMessage.message);
+  // json variant
+  res.send(errorMessage);
+});
+
 app.listen(3000);
