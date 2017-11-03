bdb75a85b17e1b5e1d13c1495c5dc769aba5b3e0
vince <vinceskahan@gmail.com>
2017-11-03 11:22:41 -0700

    promise async

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,13 +1,32 @@
-var somePromise = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    resolve('Hey it worked');
-    // reject(`ruh roh`);
-  }, 500);
+var asyncAdd = (a,b) => {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a+b);
+      } else {
+        reject('arguments must be numbers');
+      }
+    }, 1500);
+  });
+};
 
-});
-
-somePromise.then((message) => {
-  console.log('success', message);
+// geez this is needlessly slick and unreadable
+asyncAdd(1,2).then((result) => {
+  console.log(`result = `, result);
 }, (errorMessage) => {
-  console.log('error: ', errorMessage);
+  console.log(errorMessage);
 });
+
+// var somePromise = new Promise((resolve,reject) => {
+//   setTimeout(() => {
+//     resolve('Hey it worked');
+//     // reject(`ruh roh`);
+//   }, 500);
+//
+// });
+//
+// somePromise.then((message) => {
+//   console.log('success', message);
+// }, (errorMessage) => {
+//   console.log('error: ', errorMessage);
+// });
