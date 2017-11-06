618cef80e5bca64fd794f2c48903104fda213b1e
vince <vinceskahan@gmail.com>
2017-11-06 12:49:12 -0800

    example tests with "expect"

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,2 +1,9 @@
 module.exports.add = (a,b) => a+b;
 module.exports.square = (x) => x*x;
+
+module.exports.setName = (user,fullname) => {
+  var names = fullname.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,61 @@
+const expect = require('expect')
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var results = utils.add(33,11);
-  if (results !== 44) {
-    throw new Error(`expected 44 got ${results}`);
-  };
+  expect(results).toBe(44).toBeA('number');
+
+  // if (results !== 44) {
+  //   throw new Error(`expected 44 got ${results}`);
+  // };
 });
 
 it('should square a number', () => {
   var results = utils.square(3);
-  if (results !== 9) {
-    throw new Error(`expected 9 got ${results}`);
-  };
+  expect(results).toBe(9).toBeA('number');
+  // if (results !== 9) {
+  //   throw new Error(`expected 9 got ${results}`);
+  // };
 });
+
+//should verify first and last names are set
+//assert it includes first+lastname with proper values
+//and type is an object
+it('should do good', () => {
+  var user = { age: 25, location: 'philly' };
+  var results = utils.setName(user,'joe user');
+
+  // expect(results.firstName === "joe");
+  // expect(results.lastName === "user");
+  // expect(results.age === 25);
+
+  //alternately
+  expect(results).toInclude({
+    firstName: 'joe',
+    lastName: 'user',
+  });
+});
+
+// it('should expect something', () => {
+//   // expect(12).toNotBe(11);
+//
+//   // this fails because they are not the same object
+//   //expect({name: 'Andrew'}).toBe({name: 'Andrew'});
+//
+//   // this fails because they are the same object
+//   //expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+//
+//   //fails
+//   //expect([2,3,4]).toInclude(5);
+//   //works
+//   //expect([2,3,4]).toInclude(2);
+//
+//   // expect({
+//   //   name: "Andrew",
+//   //   location: 'Philly'
+//   // }).toInclude({
+//   //   location: 'Philly'
+//   // });
+//   // similarly toExclude
+//
+// });
