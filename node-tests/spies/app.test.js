d92428400d949eb07c472d26a17ffccf608fe14e
vince <vinceskahan@gmail.com>
2017-11-06 15:03:40 -0800

    example with params

--- node-tests/spies/app.test.js
+++ node-tests/spies/app.test.js
@@ -4,8 +4,11 @@ describe('App', () => {
 
   it('should call the spy correctly', () => {
     var spy = expect.createSpy();
-    spy();
-    expect(spy).toHaveBeenCalled();
+
+    // example with params
+    spy("Andrew",25);
+    expect(spy).toHaveBeenCalledWith('Andrew', 25);
+
   });
 
 });
