a03e6a2bb86cf071ead6208e4d912f4441040c63
vince <vinceskahan@gmail.com>
2017-11-06 13:59:49 -0800

    break it on purpose, send custom error code and message

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 var app = express();
 
+// app.get('/', (req,res) => {
+//   res.send('hello world');
+// });
+
 app.get('/', (req,res) => {
-  res.send('hello world');
+  //break it on purpose
+     // res.status(404).send('hello world');
+  res.status(404).send({
+    error: 'Page not found.'
+  });
 });
 
 app.listen(3000);
