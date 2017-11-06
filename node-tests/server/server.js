67b21794036ad55ee28102beff3af388988e30cf
vince <vinceskahan@gmail.com>
2017-11-06 14:18:49 -0800

    works minimally

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -6,15 +6,25 @@ var app = express();
 //   res.send('hello world');
 // });
 
-app.get('/', (req,res) => {
-  //break it on purpose
-     // res.status(404).send('hello world');
-  res.status(404).send({
-    error: 'Page not found.',
-    name: 'Todo App v1.0'
-  });
-});
+// app.get('/', (req,res) => {
+//   //break it on purpose
+//      // res.status(404).send('hello world');
+//   res.status(404).send({
+//     error: 'Page not found.',
+//     name: 'Todo App v1.0'
+//   });
+// });
 
+// GET /users and return array of users
+//           each with a name+age property
+app.get('/users', (req,res) => {
+  res.status(200).send([
+      { name: 'user 1', age: 21 },
+      { name: 'user 2', age: 22 },
+      { name: 'user 3', age: 23 },
+    ]
+  );
+});
 app.listen(3000);
 
 module.exports.app = app;
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,60 +1,60 @@
 const expect = require('expect')
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var results = utils.add(33,11);
-  expect(results).toBe(44).toBeA('number');
-
-  // if (results !== 44) {
-  //   throw new Error(`expected 44 got ${results}`);
-  // };
-});
-
-// bug - mocha thinks this passes always because async
-// returns immediately
-it('should async add two numbers', () => {
-  var results = utils.asyncAdd(4,3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-  });
-});
-
-// better...
-// add call to done() so mocha waits to be indeed done
-it('should async add two numbers', (done) => {
-  var results = utils.asyncAdd(4,3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
-  });
-});
-
-it('should square a number', () => {
-  var results = utils.square(3);
-  expect(results).toBe(9).toBeA('number');
-});
-
-it('should async square two numbers', (done) => {
-  var results = utils.asyncSquare(4, (square) => {
-    expect(square).toBe(16).toBeA('number');
-    done();
-  });
-});
-//should verify first and last names are set
-//assert it includes first+lastname with proper values
-//and type is an object
-it('should do good', () => {
-  var user = { age: 25, location: 'philly' };
-  var results = utils.setName(user,'joe user');
-
-  // expect(results.firstName === "joe");
-  // expect(results.lastName === "user");
-  // expect(results.age === 25);
-
-  //alternately
-  expect(results).toInclude({
-    firstName: 'joe',
-    lastName: 'user',
-  });
-});
+// it('should add two numbers', () => {
+//   var results = utils.add(33,11);
+//   expect(results).toBe(44).toBeA('number');
+//
+//   // if (results !== 44) {
+//   //   throw new Error(`expected 44 got ${results}`);
+//   // };
+// });
+//
+// // bug - mocha thinks this passes always because async
+// // returns immediately
+// it('should async add two numbers', () => {
+//   var results = utils.asyncAdd(4,3, (sum) => {
+//     expect(sum).toBe(7).toBeA('number');
+//   });
+// });
+//
+// // better...
+// // add call to done() so mocha waits to be indeed done
+// it('should async add two numbers', (done) => {
+//   var results = utils.asyncAdd(4,3, (sum) => {
+//     expect(sum).toBe(7).toBeA('number');
+//     done();
+//   });
+// });
+//
+// it('should square a number', () => {
+//   var results = utils.square(3);
+//   expect(results).toBe(9).toBeA('number');
+// });
+//
+// it('should async square two numbers', (done) => {
+//   var results = utils.asyncSquare(4, (square) => {
+//     expect(square).toBe(16).toBeA('number');
+//     done();
+//   });
+// });
+// //should verify first and last names are set
+// //assert it includes first+lastname with proper values
+// //and type is an object
+// it('should do good', () => {
+//   var user = { age: 25, location: 'philly' };
+//   var results = utils.setName(user,'joe user');
+//
+//   // expect(results.firstName === "joe");
+//   // expect(results.lastName === "user");
+//   // expect(results.age === 25);
+//
+//   //alternately
+//   expect(results).toInclude({
+//     firstName: 'joe',
+//     lastName: 'user',
+//   });
+// });
 
 // it('should expect something', () => {
 //   // expect(12).toNotBe(11);
