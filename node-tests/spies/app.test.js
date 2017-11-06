9736bc95de71b757641983eb0f99397775021aa4
vince <vinceskahan@gmail.com>
2017-11-06 15:11:30 -0800

    after sec6/58

--- node-tests/spies/app.test.js
+++ node-tests/spies/app.test.js
@@ -1,14 +1,26 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
 
+  var db = {
+      saveUser: expect.createSpy()
+  };
+  app.__set__('db', db);
+
   it('should call the spy correctly', () => {
     var spy = expect.createSpy();
-
-    // example with params
     spy("Andrew",25);
     expect(spy).toHaveBeenCalledWith('Andrew', 25);
+  });
 
+  it('should call SaveUser with user object', () => {
+    var email = 'me@example.com';
+    var password = '123abc';
+    app.handleSignup(email,password);
+    expect(db.saveUser).toHaveBeenCalledWith({email,password});
   });
 
 });
