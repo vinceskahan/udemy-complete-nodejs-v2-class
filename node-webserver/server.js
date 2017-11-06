f1e0489a6a57bdb36e42c895cbcc88383e50ea4c
vince <vinceskahan@gmail.com>
2017-11-06 07:26:00 -0800

    use next()

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -8,6 +8,12 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 app.use(express.static(__dirname + "/public"));
 
+app.use((request,response,next) => {
+  var now = new Date().toString();
+  console.log(`${now}:`);
+  next();
+});
+
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
