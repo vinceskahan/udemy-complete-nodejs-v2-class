037615734eb4d1216c62a8901b2387e1b83ba518
Vince Skahan <vds@uw.edu>
2017-11-07 11:10:07 -0800

    deleteOne

--- node-todo-api/playground/mongodb-delete.js
+++ node-todo-api/playground/mongodb-delete.js
@@ -10,22 +10,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   }
   console.log('connected to db');
 
-  //deleteMany
-  db.collection('Users').deleteMany({name: "somedude"}).then((result) => {
-    console.log(result);
-  });
-
-  //deleteOne
-  // db.collection('Todos').deleteOne({_id: "5a020119238f8fe5601b0d8c"}).then((result) => {
+  // //deleteMany
+  // db.collection('Users').deleteMany({name: "somedude"}).then((result) => {
   //   console.log(result);
   // });
 
-  //findOneAndDelete
-  db.collection('Users').findOneAndDelete({
-    _id: new ObjectID("5a020119238f8fe5601b0d8c")
-  }).then((results) => {
-    console.log(JSON.stringify(results,undefined,2));
+  //deleteOne
+  db.collection('Users').deleteOne({name: 'somedude2'}).then((result) => {
+    console.log(JSON.stringify(result,undefined,2));
   });
+
+  // //findOneAndDelete
+  // db.collection('Users').findOneAndDelete({
+  //   _id: new ObjectID("5a020119238f8fe5601b0d8c")
+  // }).then((results) => {
+  //   console.log(JSON.stringify(results,undefined,2));
+  // });
 
   db.close();
 });
