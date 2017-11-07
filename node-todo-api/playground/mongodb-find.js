6f04707da0484800bd35f89df687ed97b440192a
Vince Skahan <vds@uw.edu>
2017-11-07 09:08:23 -0800

    basic find

---
+++ node-todo-api/playground/mongodb-find.js
@@ -0,0 +1,22 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
+  if (err) {
+    return console.log('unable to connect to db server');
+  }
+  console.log('connected to db');
+
+  // get array of all docs
+  db.collection('Todos').find().toArray().then((docs) => {
+    console.log('todos');
+    console.log(JSON.stringify(docs,undefined,2));
+  }, (err) => {
+    console.log('unable to fetch todos', err);
+  });
+
+  //db.close();
+});
