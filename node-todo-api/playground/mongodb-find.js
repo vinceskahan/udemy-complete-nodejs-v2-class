ab0dfd6466347556e01099e72388ba457eb6f7f0
Vince Skahan <vds@uw.edu>
2017-11-07 09:15:52 -0800

    count docs

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -13,12 +13,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   // get array of all docs that are false
   //db.collection('Todos').find({completed: false}).toArray().then((docs) => {
 
+  // // get a particular document by id
+  // db.collection('Todos').find({
+  //   _id: new ObjectID("5a01e06a995f422f5cac4003")
+  // }).toArray().then((docs) => {
+  //   console.log('todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // }, (err) => {
+  //   console.log('unable to fetch todos', err);
+  // });
+
   // get a particular document by id
-  db.collection('Todos').find({
-    _id: new ObjectID("5a01e06a995f422f5cac4003")
-  }).toArray().then((docs) => {
+  db.collection('Todos').find().count().then((count) => {
     console.log('todos');
-    console.log(JSON.stringify(docs,undefined,2));
+    console.log(`todos count: ${count}`);
   }, (err) => {
     console.log('unable to fetch todos', err);
   });
