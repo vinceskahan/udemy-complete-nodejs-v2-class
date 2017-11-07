c852bc68fd7d3a400a6da0503454ee1e06217ec0
Vince Skahan <vds@uw.edu>
2017-11-07 12:15:03 -0800

    demo sec7/70

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+var newTodo = new Todo({
+  text: 'cook dinner'
+});
+
+newTodo.save().then( (doc) => {
+  console.log('saved todo: ', doc);
+}, (e) => {
+  console.log('unable to save todo');
+});
