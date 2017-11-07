ebec7356beff10717c4f272deb45836b46d75262
Vince Skahan <vds@uw.edu>
2017-11-07 10:48:19 -0800

    deleteOne

--- node-todo-api/playground/mongodb-delete.js
+++ node-todo-api/playground/mongodb-delete.js
@@ -11,12 +11,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   console.log('connected to db');
 
   //deleteMany
-  db.collection('Todos').deleteMany({text: "eat"}).then((result) => {
-    console.log(result);
-  });
+  // db.collection('Todos').deleteMany({text: "eat"}).then((result) => {
+  //   console.log(result);
+  // });
 
   //deleteOne
-
+  db.collection('Todos').deleteOne({text: "eat"}).then((result) => {
+    console.log(result);
+  });
+  
   //findOneAndDelete
 
   db.close();
