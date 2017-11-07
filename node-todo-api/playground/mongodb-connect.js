86652b7b111e8825e32afd86a586e93bf9b14fba
Vince Skahan <vds@uw.edu>
2017-11-07 09:01:06 -0800

    end of 64

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,8 +1,8 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-var obj = new ObjectID();
-console.log(obj);
+// var obj = new ObjectID();
+// console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   if (err) {
