1e03f1f040f073ab9d41b00020ca43359f21fd3d
Vince Skahan <vds@uw.edu>
2017-11-07 09:00:45 -0800

    object destructuring example to make object id

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   if (err) {
@@ -19,17 +23,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   // });
 
   //--- challenge ---
-  
-  db.collection('Users').insertOne({
-    name: 'somedude',
-    age: 25,
-    location: 'home'
-  }, (err, result) => {
-    if (err) {
-      return console.log('unable to insert user', err);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+
+  // db.collection('Users').insertOne({
+  //   name: 'somedude',
+  //   age: 25,
+  //   location: 'home'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('unable to insert user', err);
+  //   }
+  //   // example - get timestamp of created document
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   db.close();
 });
