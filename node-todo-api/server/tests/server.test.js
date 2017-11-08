dba25182bcf6ebc68356e7d0691499e4eb95050e
vince <vinceskahan@gmail.com>
2017-11-08 12:47:55 -0800

    test fails

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -1,8 +1,8 @@
 const expect = require('expect');
-const request =  require('request');
+const request =  require('supertest');
 
 const {app} = require('./../server');
-const {Todo} = require('./../modules/todo');
+const {Todo} = require('./../models/todo');
 
 // wipe all records
 beforeEach((done) => {
@@ -11,7 +11,7 @@ beforeEach((done) => {
 
 describe('POST /todos', () => {
 
-  it('should create a new todo', (done) {
+  it('should create a new todo', (done) => {
     var text = 'test string';
 
     request(app)
@@ -19,20 +19,19 @@ describe('POST /todos', () => {
       .send({text})
       .expect(200)
       .expect((res) => {
-        expect(req.body).toBe(text));
+        expect(res.body.text).toBe(text);
       })
       .end((err,res) => {
-        return done(err);
-      });
+        if (err) {
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      }); //end of .end
 
-      Todo.find().then((todos) => {
-        expect(todos.length).toBe(1);
-        expect(todos[0].text).toBe(text);
-        done();
-      }).catch((e) => {
-        done(e);
-      };
+  }); // end of it
 
-  });
-
-});
+});   // end of describe
