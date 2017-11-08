de68e21bdc8e749bda523495de178770727c2719
vince <vinceskahan@gmail.com>
2017-11-08 14:29:47 -0800

    initial queries file

---
+++ node-todo-api/playground/mongoose-queries.js
@@ -0,0 +1,16 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = "5a0383843411dec8993435f1";
+
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('todos', todos);
+});
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('todo', todo);
+});
