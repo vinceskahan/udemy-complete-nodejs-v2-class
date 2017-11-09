babba998eb97f849225f1f202a2d052304209382
vince <vinceskahan@gmail.com>
2017-11-08 16:27:38 -0800

    get challenge

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 
@@ -28,6 +30,25 @@ app.get('/todos', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+// GET /todos/12341234
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  //validate id is valid
+  //   if not 404 and send empty body
+  //findById
+  //   if todo send it back
+  //   if no todo send 404 with empty body
+  //   if err send 400 with empty body
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send("");
+    };
+    res.status(200).send(JSON.stringify(todo,undefined,2));
+    }).catch((e) => {
+      res.status(400).send("");
+    });
 });
 
 // start up the server
