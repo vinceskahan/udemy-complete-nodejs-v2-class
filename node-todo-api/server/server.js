8102aeda14033ecf631bf1b6ae0eaa5fe19e8e89
Vince Skahan <vds@uw.edu>
2017-11-09 10:43:59 -0800

    return object in delete

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -70,7 +70,7 @@ app.delete('/todos/:id', (req,res) => {
     }
     Todo.findByIdAndRemove(id).then((todo) => {
       if (!todo) {
-          return res.status(404).send();
+          return res.status(404).send({todo});
       };
       res.status(200).send({todo});
       }).catch((e) => {
