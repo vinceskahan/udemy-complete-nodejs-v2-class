be31b3c6df90a39dc40dd1ebb1a3d3a22f8752ec
vince <vinceskahan@gmail.com>
2017-11-09 16:54:27 -0800

    end of sec7/86 to-do notes api

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,12 +1,4 @@
-var env = process.env.NODE_ENV || "development";
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
-
+var config = require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
