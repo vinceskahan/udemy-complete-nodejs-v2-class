d100d19a806d1ff16c37fc21dd46fd68bb015408
vince <vinceskahan@gmail.com>
2017-11-14 15:51:17 -0800

    after sec8/93

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,9 +4,13 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
-const {todos, populateTodos} = require('./seed/seed');
+const {User} = require('./../models/user');
 
+const {todos, populateTodos,  users, populateUsers} = require('./seed/seed');
+
+beforeEach(populateUsers);
 beforeEach(populateTodos);
+
 
 //------ POST tests --------
 
