5dbc2c60ad775f0d9d418be815fab50548f892a4
Vince Skahan <vds@uw.edu>
2017-11-14 10:50:09 -0800

    hash example

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,7 +1,25 @@
 const {SHA256} = require('crypto-js');
 
-var message = 'this is a string';
+var message = 'I am user number 3';
 var hash = SHA256(message).toString();
 
 console.log(`message: ${message}`);
 console.log(`hash: ${hash}`);
+
+var data = {
+  id: 4
+};
+
+// salt the data we're hashing
+var token = {
+  data,
+  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+};
+var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+if (resultHash === token.hash) {
+  console.log('data was not changed');
+} else {
+  console.log('data was changed - do not trust');
+  console.log(`data:  {{resulthash}}`);
+  console.log(`token: {{token.hash}}`);
+}
