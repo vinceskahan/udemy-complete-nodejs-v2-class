c530e90d6433bd7ac96db2a2f2be8682a575aa5b
Vince Skahan <vds@uw.edu>
2017-11-14 10:51:03 -0800

    same, fake impersonate without salting hash

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -15,11 +15,14 @@ var token = {
   data,
   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
 };
+
+// fake impersonation without salt
+token.data.id=5;
+token.hash = SHA256(JSON.stringify(data)).toString();
+
 var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
 if (resultHash === token.hash) {
   console.log('data was not changed');
 } else {
   console.log('data was changed - do not trust');
-  console.log(`data:  {{resulthash}}`);
-  console.log(`token: {{token.hash}}`);
 }
