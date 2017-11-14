2d5bf6f4511202c877dc98e71755ef87b06af21d
Vince Skahan <vds@uw.edu>
2017-11-14 11:56:36 -0800

    before refactoring into separate files

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -43,18 +43,25 @@ app.post('/users', (req,res) => {
   })
 });
 
-// private route to GET my user info specificatlly
-app.get('/users/me', (req,res) => {
+var authenticate = (req,res,next) => {
   var token = req.header('x-auth');
   User.findByToken(token).then((user) => {
     if (!user) {
       // run the error case here to be DRYer
       return Promise.reject();
     }
-    res.send(user);
+    //modify req object
+    req.user = user;
+    req.token = token;
+    next();   // so the req.send in get users/me executes
   }).catch((e) => {
     res.status(401).send();    // auth required status code
   });
+};
+
+// private route to GET my user info specifically
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
 });
 
 // GET
