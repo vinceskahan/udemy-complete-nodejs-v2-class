8abb921b27fe3dd7df50ad5320c502de3bee5d7c
Vince Skahan <vds@uw.edu>
2017-11-14 10:52:27 -0800

    comment out crypto-js example

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,28 +1,28 @@
 const {SHA256} = require('crypto-js');
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
-
-console.log(`message: ${message}`);
-console.log(`hash: ${hash}`);
-
-var data = {
-  id: 4
-};
-
-// salt the data we're hashing
-var token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-};
-
-// fake impersonation without salt
-// token.data.id=5;
-// token.hash = SHA256(JSON.stringify(data)).toString();
-
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-if (resultHash === token.hash) {
-  console.log('data was not changed');
-} else {
-  console.log('data was changed - do not trust');
-}
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`message: ${message}`);
+// console.log(`hash: ${hash}`);
+//
+// var data = {
+//   id: 4
+// };
+//
+// // salt the data we're hashing
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+//
+// // fake impersonation without salt
+// // token.data.id=5;
+// // token.hash = SHA256(JSON.stringify(data)).toString();
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// if (resultHash === token.hash) {
+//   console.log('data was not changed');
+// } else {
+//   console.log('data was changed - do not trust');
+// }
