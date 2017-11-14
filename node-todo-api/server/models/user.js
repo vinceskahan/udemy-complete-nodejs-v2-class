af8ad1f3a066b3684e0693fc6460a8252fd1207a
Vince Skahan <vds@uw.edu>
2017-11-14 11:49:39 -0800

    simplify reject

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -62,9 +62,7 @@ UserSchema.statics.findByToken = function (token) {
   try {
     decoded = jwt.verify(token, 'abc123');
   } catch (e) {
-    return new Promise((resolve,reject) => {
-      reject();
-    });
+    return Promise.reject();
   }
 
   // success of try/catch
