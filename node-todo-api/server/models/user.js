b9446db560f1f4e9e79896699ea6c327fabf3389
Vince Skahan <vds@uw.edu>
2017-11-14 11:33:38 -0800

    end of sec 8/90

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -49,13 +49,10 @@ UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
-
   user.tokens.push({access,token});
   return user.save().then(() => {
     return token;
   });
-
-  })
 };
 
 var User = mongoose.model('User', UserSchema);
