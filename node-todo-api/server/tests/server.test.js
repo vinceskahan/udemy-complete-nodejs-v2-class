6c0b6487a12835d8ac88ba084913132e5fad5bc3
vince <vinceskahan@gmail.com>
2017-11-20 12:19:45 -0800

    catch up tests

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -21,6 +21,7 @@ describe('POST /todos', () => {
     var text = 'test string';
     request(app)
       .post('/todos')
+      .set('x-auth', users[0].tokens[0].token)
       .send({text})
       .expect(200)
       .expect((res) => {
@@ -42,6 +43,7 @@ describe('POST /todos', () => {
     var text = '';
     request(app)
       .post('/todos')
+      .set('x-auth', users[0].tokens[0].token)
       .send({text})
       .expect(400)
       .end((err,res) => {
@@ -59,12 +61,13 @@ describe('POST /todos', () => {
 
 describe('GET /todos', () => {
 
-  it('should get all todos', (done) => {
+  it('should get all todos for the authenticated user', (done) => {
     request(app)
       .get('/todos')
+      .set('x-auth', users[0].tokens[0].token)
       .expect(200)
       .expect((res) => {
-        expect(res.body.todos.length).toBe(2);
+        expect(res.body.todos.length).toBe(1);
       })
       .end(done);
   });
