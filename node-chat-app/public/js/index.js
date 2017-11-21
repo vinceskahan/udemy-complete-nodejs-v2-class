3754d207b19e5ceed364388efc1b89211d235758
vince <vinceskahan@gmail.com>
2017-11-21 13:02:37 -0800

    before challenge

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,8 +1,22 @@
 // establish socket and log to client console
 var socket = io();
+
 socket.on('connect', function () {
   console.log('connected to server');
+
+  // create email event from client to server, with data after connect
+  socket.emit('createEmail', {
+    to: 'jen@example.com',
+    text: 'hey, this is me'
+  });
+
 });
+
 socket.on('disconnect', function () {
   console.log('disconnected from server');
+});
+
+// custom event from server to client
+socket.on('newEmail', function (email) {
+  console.log('New email',email);
 });
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 // register listener
 io.on('connection', (socket) => {
   console.log('new user connected');
+
+  // emit an event called in index.js
+  socket.emit('newEmail', {
+    from: 'mike@example.com',
+    text: 'Hey whassup?',
+    createAt: 123
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+  
   socket.on('disconnect', (socket) => {
     console.log('new user disconnected');
   });
