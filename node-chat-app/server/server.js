9640fce50e4ddbeece8ae392f519ea36dff19be6
vince <vinceskahan@gmail.com>
2017-11-21 08:43:06 -0800

    add heroku compatibility

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname,'../public');
-const port = 3000;
+const port = process.env.PORT || 3000; // for heroku
 
 // create new express app
 // - configure it to server up the 'public' folder
@@ -12,8 +12,6 @@ const port = 3000;
 var app = express();
 
 app.use(express.static(publicPath));
-
-//app.get('/index.html')
 
 app.listen(port, () => {
   console.log(`started up at port ${port}`);
