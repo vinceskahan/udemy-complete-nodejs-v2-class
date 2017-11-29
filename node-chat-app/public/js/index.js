5fc2d80f4615d255142eae4cf1dbfab71dff2d00
vince <vinceskahan@gmail.com>
2017-11-29 10:17:53 -0800

    messageTextbox, clear input

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -31,11 +31,14 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+
+  var messageTextbox =  jQuery('[name=message]')
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    // acknowledgement callback
+    messageTextbox.val("")
   });
 });
 
