92acbc832e49164e10c437d880115326487b0614
vince <vinceskahan@gmail.com>
2017-11-28 16:30:31 -0800

    code works

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -16,9 +16,21 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  console.log('newLocationMessage: ', message);
+  var li = jQuery('<li></li>');
+  // open in a new tab when the link is clicked on
+  var a = jQuery('<a target="_blank">My current location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
-
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name=message]').val()
@@ -33,18 +45,17 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
+
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
-  }, function (error) {  
+    console.log(`console log: lat=${position.coords.latitude} lon=${position.coords.longitude}`)
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+      alert('unable to fetch location');
   },{
+    // needed it seems for some browsers and operating systems
     enableHighAccuracy: true
   });
-});s
-  // navigator.geolocation.getCurrentPosition(function (position) {
-  //   // socket.emit('createLocationMessage', {
-  //   //   latitude: position.coords.latitude,
-  //   //   longitude: position.coords.longitude
-  //   // });
-  // }, function () {
-  //   alert('Unable to fetch location.');
-  // });
+});
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000; // for heroku
 var app = express();
@@ -18,29 +18,15 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-  // ---- emit to a single connection
-  // socket.emit('newMessage', {
-  //   from: 'myserver@example.com',
-  //   text: 'New message from server',
-  //   createdAt: 123
-  // });
 
-  // challenge
-  // -- socket.emit from admin text='welcome to the chat app'
-  // -- socket.broadcast.emit from admin text='new user joined'
- 
-  // createMessage listener
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server.');
+  });
 
-    // emit to everybody 'except' ourselves
-              // socket.broadcast.emit('newMessage', {
-              //   from: message.from,
-              //   text: message.text,
-              //   createdAt: new Date().getTime()
-              // });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude,coords.longitude));
   });
 
   socket.on('disconnect', () => {
@@ -51,4 +37,3 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
-
