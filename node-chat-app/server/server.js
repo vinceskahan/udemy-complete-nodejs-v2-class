5b4f6bd9847c074d15aa1910c694538993d6f4c0
vince <vinceskahan@gmail.com>
2017-12-08 12:08:52 -0800

    emit to members of same room only

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -16,14 +16,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  // socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  // socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
       callback('Name and room name are required');
     }
+
+    socket.join(params.room);
+    // socket.leave('your name here');
+
+    // ways we've emitted before:
+    //   io.emit - emits to all connected users
+    //   socket.broadcast.emit - to all connected users except yourself
+    //   socket.emit - to a specific user
+
+    // we could more specifically emit to a specific room
+    //  io.to(params.room).emit;
+    //  socket.broadcast.to(params.room);
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+
     callback();
   });
 
