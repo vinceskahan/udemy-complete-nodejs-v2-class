4364f7c11be7b3f3fdf6a02d2bc4003382851876
vince <vinceskahan@gmail.com>
2017-12-11 10:47:34 -0800

    initial converter

--- async-await/currency-convert.js
+++ async-await/currency-convert.js
@@ -13,14 +13,39 @@ const getExchangeRate = (from, to) => {
     return response.data.rates[to];
   });
 }
-const getCountries = (currentncyCode) => {
-  axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
-    return response.data.rates[to];
+
+// getExchangeRate('USD','CAD').then((rate) => {
+//   console.log(rate);
+// }).catch((e) => {
+//   console.log(`error ${e}`);
+// });
+
+
+const getCountries = (currencyCode) => {
+  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+    // return array with countries accepting this code as array of strings
+    return response.data.map((country) => country.name);
   });
 }
 
-getExchangeRate('USD','CAD').then((rate) => {
-  console.log(rate);
+// getCountries('EUR').then((countries) => {
+//   console.log(countries);
+// }).catch((e) => {
+//   console.log(`error ${e}`);
+// });
+
+const convertCurrency = (from,to,amount) => {
+  return getCountries(to).then(countries => {
+    return getExchangeRate(from,to);
+  }).then((rate) => {
+    const exchangedAmount = amount * rate;
+
+    return `${amount} in ${from} is worth ${exchangedAmount} ${to}.`;
+  });
+};
+
+convertCurrency('CAD','USD', 100).then((status) => {
+  console.log(status);
 }).catch((e) => {
   console.log(`error ${e}`);
 });
