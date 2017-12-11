b6f19a7d7c109a8fa447635c281df1ff586dea4e
vince <vinceskahan@gmail.com>
2017-12-11 08:35:00 -0800

    getGrades

--- async-await/async-await.js
+++ async-await/async-await.js
@@ -8,7 +8,19 @@ const users = [{
   schoolId: 999
 }];
 
-const grades = [];
+const grades = [{
+  id: 1,
+  schoolId: 101,
+  grade: 86
+},{
+  id: 2,
+  schoolId: 999,
+  grade: 100
+},{
+  id: 3,
+  schoolId: 101,
+  grade: 80
+}];
 
 const getUser = (id) => {
   return new Promise((resolve,reject) => {
@@ -21,8 +33,30 @@ const getUser = (id) => {
   });
 };
 
-getUser(2).then((user) => {
-  console.log(user);
+const getGrades = (schoolId) => {
+  return new Promise((resolve,reject) => {
+    resolve(grades.filter((grade) => grade.schoolId === schoolId));
+  });
+};
+
+// const getUser = (id) => {
+//   return new Promise((resolve,reject) => {
+//     const user = users.find((user) => user.id === id);   // return user if id matches
+//     if (user) {
+//         resolve(user);
+//     } else {
+//       reject(`unable to find user with id of ${id}.`);
+//     }
+//   });
+// };
+//
+// getUser(2).then((user) => {
+//   console.log(user);
+// }).catch((err) => {
+//   console.log(err);
+// });
+getGrades(101).then((grades) => {
+  console.log(grades);
 }).catch((err) => {
   console.log(err);
 });
